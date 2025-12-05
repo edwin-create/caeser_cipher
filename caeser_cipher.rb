99e7208d36837f284eb526c9ad1815b1e6628b2a
edwin-create <edwinwanyoike9999@gmail.com>
2025-12-05 18:44:13 +0300

    Introduced rubocop-performance gem Configured .rubocop.yml to enable new cops.

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -4,15 +4,15 @@ require 'pry-byebug'
 
 def caesar_cipher(string, shift) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
   # Split the input string into an array of individual characters for processing
-  string_sub_strings = string.split('')
+  string_sub_strings = string.chars
 
   # Map each character to its shifted ASCII value (or original ASCII for non-letters)
   # Check if character is a lowercase / uppercase letter
   # Convert to 0-based position (a=0), apply shift, wrap with modulo 26, convert back
 
   character_ascii_value = string_sub_strings.map do |character|
-    if character =~ /[a-z]/ then ((character.ord - 97 + shift) % 26 + 97)
-    elsif character =~ /[A-Z]/ then ((character.ord - 65 + shift) % 26 + 65)
+    if /[a-z]/.match?(character) then (((character.ord - 97 + shift) % 26) + 97)
+    elsif /[A-Z]/.match?(character) then (((character.ord - 65 + shift) % 26) + 65)
     else
       character.ord # Return ASCII value of non-alphabetic characters (will be converted back unchanged)
     end
@@ -22,7 +22,7 @@ def caesar_cipher(string, shift) # rubocop:disable Metrics/AbcSize,Metrics/Metho
   character_chr = character_ascii_value.map(&:chr)
 
   # Join the character array into a single string
-  character_chr = character_chr.join('')
+  character_chr = character_chr.join
   binding.pry # rubocop:disable Lint/Debugger
 end
 
